7b9e1a0daf6de327bc5cc80eb04990018823bd27
kathyqat <k_qin@hotmail.com>
2020-06-18 19:07:55 -0400

    Add functional display when number and clear buttons are pressed

--- script.js
+++ script.js
@@ -17,3 +17,21 @@ function divide (x, y){
 function operate (operator, x, y){
     return operator(x, y);
 }
+
+const numberButtons = document.querySelectorAll('.number');
+const display = document.querySelector('#display');
+const clearButton = document.querySelector('#AC')
+let displayValue = '';
+
+numberButtons.forEach((button) => {
+  button.addEventListener('click', () => {
+    let number = button.getAttribute('id');
+    displayValue += number;
+    display.textContent = displayValue;
+  });
+});
+
+clearButton.addEventListener('click', () => {
+  displayValue = '';
+  display.textContent = '';
+});
