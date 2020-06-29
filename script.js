99858b8244cd58973291b7556ffcef02744d594d
kathyqat <k_qin@hotmail.com>
2020-06-29 19:28:26 -0400

    Add evaluation of BEDMAS

--- script.js
+++ script.js
@@ -22,75 +22,106 @@ const display = document.querySelector('#display');
 const clearButton = document.querySelector('#AC');
 let displayValue = '';
 let operation = '';
-let num1, num2;
+let mathArray = [];
 
 function inputNumbers(){
     const numberButtons = document.querySelectorAll('.number');
-
+    
     numberButtons.forEach((button) => {
-      button.addEventListener('click', () => {
-        let number = button.getAttribute('id');
-
-        if (displayValue.length < 34){
-            displayValue += number;
-        };
-        display.textContent = displayValue;
-        display.style.borderColor = '#4285f4';
-      });
+        button.addEventListener('click', () => {
+            let number = button.getAttribute('id');
+            
+            if (displayValue.length < 34){
+                displayValue += number;
+            };
+            display.textContent = displayValue;
+            display.style.borderColor = '#4285f4';
+        });
     });
-  }
+}
+
+function determineOperator(action){
+    switch (action){
+        case '/':
+            operation = divide;
+            break;
+        case '*':
+            operation = multiply;
+            break;
+        case '-':
+            operation = subtract;
+            break;
+        case '+':
+            operation = add;
+            break;
+        default:
+    };
+}
 
 function inputAction(){
     const actionButtons = document.querySelectorAll('.action');
-    
+
     actionButtons.forEach((button) => {
         button.addEventListener('click', () => {
-            num1 = Number(displayValue);
-            displayValue = '';
             operation = button.getAttribute('id');
-
-            switch (operation){
-                case '/':
-                operation = divide;
-                break;
-                case '*':
-                operation = multiply;
-                break;
-                case '-':
-                operation = subtract;
-                break;
-                case '+':
-                operation = add;
-                break;
-                default:
+            
+            if (operation != "AC"){
+                let num = Number(displayValue);
+                displayValue = '';
+                mathArray.push(num);
+                mathArray.push(operation);
             };
         });
     });
 }
 
-function pressEquals(){
+function reduceArray(index){
+    determineOperator(mathArray[index]);
+    let result = operate(operation, mathArray[index-1], mathArray[index+1]);
+    mathArray.splice(index-1, 3, result);
+}
+
+function evaluateEquals(){
     const equalsButton = document.querySelector('.equals');
     
     equalsButton.addEventListener('click', () => {
         if ((operation == '') || ((operation) && (displayValue == ''))){
             return;
         } else {
-            num2 = Number(displayValue);
+            let num = Number(displayValue);
             displayValue = '';
+            mathArray.push(num);
             
-            if ((operation == divide) && (num2 == 0)) {
-                display.textContent = 'ILLEGAL MOVE';
-            } else {
-                let result = operate(operation, num1, num2);
-                result = result.toString();
-
-                if (result.length > 34){
-                    result = result.slice(0, 34);
-                    result = Math.round(result * 10**31) / 10**31;
-                };
-                display.textContent = result;
-                operation = '';
+            for (let i=0; i<mathArray.length; i++){
+                if ((mathArray[i] == "/") && (mathArray[i+1] ==  0)) {
+                    display.textContent = 'ILLEGAL MOVE';
+                    operation = '';  
+                    mathArray = [];
+                    return;
+                } else if ((mathArray[i] == "/") || (mathArray[i] == "*")){
+                    reduceArray(i);
+                    i = --i;
+                };    
             };
+            
+            for (let i=0; i<mathArray.length; i++){
+                if ((mathArray[i] == "+") || (mathArray[i] == "-")){
+                    reduceArray(i);
+                    i = --i;
+                };    
+            };
+            
+            let result = mathArray;
+            result = result.toString();
+                
+            if (result.length > 33){
+                result = result.slice(0, 33);
+                result = Math.round(result * 10**30) / 10**30;
+            };
+            
+            display.textContent = result;
+            operation = '';  
+            mathArray = [];
         };
     });
 }
@@ -98,6 +129,7 @@ function pressEquals(){
 clearButton.addEventListener('click', () => {
     displayValue = '';
     operation = '';
+    mathArray = [];
     display.textContent = '';
     display.style.borderColor = 'lightgrey';
 });
@@ -105,7 +137,7 @@ clearButton.addEventListener('click', () => {
 function calculate(){
     inputNumbers();
     inputAction();
-    pressEquals();
+    evaluateEquals();
 }
 
 calculate();
